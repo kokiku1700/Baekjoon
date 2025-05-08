781092c7d96231c2c5b4bdf465b247cd483da3d2
DongHyeon <104422253+kokiku1700@users.noreply.github.com>
2025-05-08 16:00:08 +0900

    [Silver IV] Title: ATM, Time: 96 ms, Memory: 9640 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.js
+++ 백준/Silver/11399. ATM/ATM.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-let n = +input[0];
-let newArr = input[1].split(' ').map(Number).sort((a, b) => a - b);
-let arr = new Array(n).fill(0);
-arr[0] = newArr[0];
+const arr = input[1].split(' ').map(Number);
+let acc = 0;
+let result = 0;
 
-for ( let i = 1; i < n; i++ ) {
-    arr[i] = newArr[i] + arr[i - 1];
-}
+arr.sort((a, b) => a - b);
 
-let result = arr.reduce((a, b) => a + b);
-console.log(result)
+arr.forEach(e => {
+    acc += e;
+    result += acc;
+});
+
+console.log(result);
