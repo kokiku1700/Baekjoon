c92f96785c501a496f4f1c27df9726c8cc890261
DongHyeon <104422253+kokiku1700@users.noreply.github.com>
2025-05-08 15:24:44 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.js
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -1,18 +1,14 @@
 const fs = require('fs');
-const input = +fs.readFileSync('/dev/stdin').toString().trim();
-let five = Math.floor(input / 5)
-let result = -1;
+const input = +fs.readFileSync('/dev/stdin').toString().trim();let result = 0;
 
-while ( five >= 0 ) {
-    let remain = input - five * 5;
-
-    if ( remain % 3 === 0 ) {
-        result = remain / 3 + five;
-        break;
-    } else {
-        five--;
-    }
+if ( input === 4 || input === 7 ) {
+    result = -1;
+} else if ( input % 5 === 0 ) {
+    result = input / 5;
+} else if ( input % 5 === 1 || input % 5 === 3 ) {
+    result = Math.floor(input / 5) + 1;
+} else if ( input % 5 === 2 || input % 5 === 4 ) {
+    result = Math.floor(input / 5) + 2
 }
-
 
 console.log(result);
