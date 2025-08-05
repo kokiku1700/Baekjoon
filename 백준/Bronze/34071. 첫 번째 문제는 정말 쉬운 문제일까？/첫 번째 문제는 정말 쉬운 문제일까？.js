a00c98c6caf76a624c326438b3d4412e19d581dc
DongHyeon <104422253+kokiku1700@users.noreply.github.com>
2025-08-05 13:46:12 +0900

    [Bronze IV] Title: 첫 번째 문제는 정말 쉬운 문제일까?, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/34071. 첫 번째 문제는 정말 쉬운 문제일까？/첫 번째 문제는 정말 쉬운 문제일까？.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
+input.shift();
+const max = Math.max(...input);
+const min = Math.min(...input);
+
+if ( input[0] === max ) {
+    console.log("hard");
+} else if ( input[0] === min ) {
+    console.log("ez");
+} else {
+    console.log("?");
+};
