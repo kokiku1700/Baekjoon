c387ccaf87354b49ed5b05c8a4f8f5ec07d0ae60
DongHyeon <104422253+kokiku1700@users.noreply.github.com>
2025-10-06 23:58:03 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 160 ms, Memory: 10084 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const [a, b, c, d, e, f] = fs.readFileSync(0, 'utf-8').toString().trim().split(' ').map(Number);
+for (let i = -999; i <= 999; i++) {
+    for (let j = -999; j <= 999; j++) {
+        if (a * i + b * j === c && d * i + e * j === f) {
+            console.log(i, j);
+            return;
+        }
+    }
+}
