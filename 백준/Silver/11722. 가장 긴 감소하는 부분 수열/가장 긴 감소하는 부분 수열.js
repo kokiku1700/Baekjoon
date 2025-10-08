30d120f6c4a4bbab8289634fb2eaeb85a83dce76
DongHyeon <104422253+kokiku1700@users.noreply.github.com>
2025-10-09 00:46:01 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 164 ms, Memory: 11596 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const n = +input[0];
+const arr = input[1].split(' ').map(Number);
+const dp = new Array(n).fill(1);
+
+for ( let i = 1; i < n; i++ ) {
+    let inArr = [1];
+
+    for ( let j = 0; j < i; j++ ) {
+        if ( arr[j] > arr[i] ) {
+            inArr.push(dp[j] + 1);
+        };
+    };
+    dp[i] = Math.max(...inArr);
+};
+
+console.log(Math.max(...dp));
